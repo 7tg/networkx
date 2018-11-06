bb1cc6ae73482267b901c61f9185b889384071dc
Tg <tayyipgoren@gmail.com>
2018-11-06 15:05:42 +0300

    Done

--- forbes/forbes.py
+++ forbes/forbes.py
@@ -1,4 +1,7 @@
 from bs4 import BeautifulSoup
+import pickle
+import os
+import networkx as nx
 
 class Rich():
     """Rich class for forbes"""
@@ -12,7 +15,7 @@ class Rich():
         self.country = country
 
     def __str__(self):
-        return str(self.rank + " -- " + self.name + " " + self.worth)
+        return str(self.rank + " -- " + self.name + " " + str(self.worth) + " --> " + str(self.source))
 
 
 with open('forbes.html', 'r') as _file:
@@ -32,15 +35,68 @@ with open('forbes.html', 'r') as _file:
             elif counter == 2:
                 name = colum.text
             elif counter == 3:
-                worth = colum.text
+                worth = float(colum.text.split(" ")[0][1:])
             elif counter == 4:
-                age = colum.text
+                age = colum.text.replace(" ", "")
             elif counter == 5:
-                source = [text.strip() for text in colum.text.split(",")]
+                source = [text.strip().lower() for text in colum.text.split(",")]
             elif counter == 6:
                 country = colum.text
             counter += 1
         richs.append(Rich(name, worth, rank, age, source, country))
 
     for rich in richs:
-        print(rich)
+        pass
+        #print(rich)
+
+def get_edges(richs):
+    partions = dict()
+
+    for rich in richs:
+        for src in rich.source:
+            if not partions.get(src):
+                partions[src] = list()
+                partions[src].append(rich.name)
+            else:
+                partions[src].append(rich.name)
+
+    return partions
+
+def edge_to_tuple(richs_edges):
+    edge_tuple_list = list()
+    for key, value in richs_edges.items():
+        for edge in value:
+            for connect_edge in value:
+                if not edge == connect_edge:
+                    edge_tuple_list.append((edge, connect_edge))
+    return edge_tuple_list
+
+def get_nodes(richs):
+    nodes = list()
+    for rich in richs:
+        nodes.append(rich.name)
+    return nodes
+
+# for rich in richs:
+#     print(rich)
+
+
+# nodes = get_nodes(richs)
+rich_edges = get_edges(richs)
+rich_edges_tuple = edge_to_tuple(rich_edges)
+
+G = nx.Graph()
+
+for rich in richs:
+    G.add_node(rich.name, worth=rich.worth, age=rich.age, rank=rich.rank, country=rich.country)
+
+G.add_edges_from(rich_edges_tuple)
+
+print(G.number_of_nodes())
+print(G.number_of_edges())
+
+nx.write_gexf(G, "forbes.gexf")
+# for edge in rich_edges_tuple:
+#     print(edge)
+# for key, value in rich_edges.items():
+#     print(key + " --> " + str(value))
